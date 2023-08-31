9fdc4af67e1e8d44e480014c7d752f2fd7e650c8
Ransom Williams <auvergnerw@gmail.com>
2023-08-31 17:48:34 -0500

    lint

--- test/test_start.py
+++ test/test_start.py
@@ -1,7 +1,8 @@
 def test_hello():
     """
-    hello world for tests.
-    this is here for now to make
+    "Hello world" for tests.
+
+    This is here for now to make
     sure that github actions has a test to run
     """
     assert True
