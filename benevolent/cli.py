34298bcdf949cfcc04db7944942d4aa313aeb1ce
Ransom Williams <auvergnerw@gmail.com>
2023-09-01 20:18:07 -0500

    lint

--- benevolent/cli.py
+++ benevolent/cli.py
@@ -3,18 +3,19 @@ from PIL import Image
 
 from benevolent.xor_enconder import create_xor_code
 
+
 @click.group()
 def cli():
-    """command-line interface root"""
+    """Command-line interface root"""
     pass
+
 
 @cli.command()
 @click.option("--path1", required=True, type=click.Path(exists=True))
 @click.option("--path2", required=True, type=click.Path(exists=True))
 @click.option("--path-out", required=True, type=click.Path(exists=False))
 def xor_code(path1, path2, path_out):
-    """xor two images together and store them as an output image"""
+    """Exclusive or (xor) two images together"""
     with Image.open(path1) as image1, Image.open(path2) as image2:
         image_out = create_xor_code(image1, image2)
         image_out.save(path_out)
-
