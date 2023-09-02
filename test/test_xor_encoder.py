4f8c5a21d4a763aea4c5845615763c4bb74a71f7
bufferbit <143241661+bufferbit@users.noreply.github.com>
2023-09-02 17:57:03 +0000

    Update unit tests
    ValueError is already raised when the images are the wrong size, so
    there is no reason to raise it ourselves. Pytest also does regex pattern
    matching on the exception message so we don't need to assert it either.

--- test/test_xor_encoder.py
+++ test/test_xor_encoder.py
@@ -33,9 +33,8 @@ def image3():
 
 def test_different_sized_images(image1, image3):
     """Test that the proper exception is thrown when trying to use two different sized images."""
-    with pytest.raises(ValueError) as excinfo:
+    with pytest.raises(ValueError, match="operands could not be broadcast together with shapes"):
         create_xor_code(image1, image3)
-    assert "Input images must have the same size for XOR operation." in str(excinfo.value)
 
 
 def test_same_xor_both_ways(image1, image2):
