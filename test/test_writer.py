0de24aff9891288be0bfea9449046616f2d8ec4b
Bautista Dalmasso <j.bauti.d@gmail.com>
2023-09-04 18:57:03 -0300

    Fix weirdness with test and linting.

--- test/test_writer.py
+++ test/test_writer.py
@@ -1,19 +1,11 @@
 from test.utils import equal_images
 
-import shutil
-from PIL import Image
-
 from benevolent.writer import Writer
 
 
-def test_write_hello_world(tmp_path, image1_bmp, image1_hello_world_bmp):
+def test_write_hello_world(image1_bmp, image1_hello_world_bmp):
     """Write hello world to an image"""
-    path = tmp_path / "writer_result.bmp"
     writer1 = Writer(image1_bmp, 36)
     writer1.write_text_box("hello world", (0, 0))
-    image1_bmp.save(path)
 
-    shutil.copyfile(path, "out.bmp")
-
-    # with Image.open(path) as result:
-    #     assert equal_images(image1_hello_world_bmp, result)
+    assert equal_images(image1_hello_world_bmp, image1_bmp)
