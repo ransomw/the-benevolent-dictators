3b44804ef3b2cbc38643a979d9f129924c4c3740
Ransom Williams <auvergnerw@gmail.com>
2023-09-03 22:47:18 -0500

    init test_xor_decode

--- test/test_click_commands.py
+++ test/test_click_commands.py
@@ -15,3 +15,19 @@ def test_xor_image_is_created(tmp_path, xord_image):
 
     with Image.open(path) as result:
         assert equal_images(xord_image, result)
+
+
+def test_xor_decode(tmp_path):
+    xor_result_path = tmp_path / "xor_result.bmp"
+    xor_decode_path = tmp_path / "xor_decode.bmp"
+    CliRunner().invoke(xor_code, ["test/images/acolchado.bmp",
+                                  "test/images/sheet.bmp",
+                                  str(xor_result_path.resolve())])
+
+    CliRunner().invoke(xor_code, ["test/images/acolchado.bmp",
+                                  str(xor_result_path.resolve()),
+                                  str(xor_decode_path.resolve())])
+
+    with Image.open("test/images/sheet.bmp") as sheet_image, Image.open(xor_decode_path) as decoded_image:
+        assert equal_images(sheet_image, decoded_image)
+
