e52ac3b1fbdea3f6fba49ea6d42bf4395aaab345
Bautista Dalmasso <j.bauti.d@gmail.com>
2023-09-04 11:01:11 -0300

    Add function to write text inside a box in an image.

--- benevolent/writer.py
+++ benevolent/writer.py
@@ -1,6 +1,16 @@
 from PIL import Image, ImageDraw, ImageFont
 
 
+def write_text_box(text: str, size: int, xy: tuple[float, float], image: Image.Image) -> Image.Image:
+    """Writes text inside a box."""
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("fonts/FreeMono.ttf", size)
+
+    draw.rectangle(draw.textbbox(xy, text, font), fill=(0, 0, 0), outline=(0, 0, 0))
+
+    write_text(text, size, xy, image)
+
+
 def write_text(text: str, size: int, xy: tuple[float, float], image: Image.Image) -> Image.Image:
     """Writes the input text at the coordinates xy."""
     draw = ImageDraw.Draw(image)
