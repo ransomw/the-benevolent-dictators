4891c9620f2fb9277b9d8c7627c2a62fe323a20f
Ransom Williams <auvergnerw@gmail.com>
2023-09-04 21:04:24 -0500

    wip

--- test/test_writer.py
+++ test/test_writer.py
@@ -9,8 +9,7 @@ def test_write_hello_world(tmp_path, image1, image1_hello_world):
     """Write hello world to an image"""
     path = tmp_path / "writer_result.bmp"
     writer1 = Writer(image1, 36)
-    writer1.write_text_box("hello world",
-                           (image1.size[0]//2, image1.size[1]//2))
+    writer1.write_text_box("hello world", (0, 0))
     image1.save(path)
 
     with Image.open(path) as result:
