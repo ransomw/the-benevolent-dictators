b4992d0ab95d3f3103d2938bd6c83b1af33994ac
Ransom Williams <auvergnerw@gmail.com>
2023-09-04 21:18:16 -0500

    upload image artifact

--- test/test_writer.py
+++ test/test_writer.py
@@ -1,5 +1,6 @@
 from test.utils import equal_images
 
+import shutil
 from PIL import Image
 
 from benevolent.writer import Writer
@@ -11,6 +12,8 @@ def test_write_hello_world(tmp_path, image1_bmp, image1_hello_world_bmp):
     writer1 = Writer(image1_bmp, 36)
     writer1.write_text_box("hello world", (0, 0))
     image1_bmp.save(path)
+
+    shutil.copyfile(path, "out.bmp")
 
     with Image.open(path) as result:
         assert equal_images(image1_hello_world_bmp, result)
