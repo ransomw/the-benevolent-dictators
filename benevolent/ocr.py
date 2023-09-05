e3abb0e0df010cb9b7216aa2cb3b661b6319fa6d
Ransom Williams <auvergnerw@gmail.com>
2023-09-04 23:25:19 -0500

    lint

--- benevolent/ocr.py
+++ benevolent/ocr.py
@@ -1,7 +1,7 @@
 import pytesseract
 from PIL import Image
 
+
 def get_image_text(image: Image.Image) -> str:
     """Returns text from an image"""
     return pytesseract.image_to_string(image)
-
--- test/test_ocr.py
+++ test/test_ocr.py
@@ -1,7 +1,7 @@
 from benevolent.ocr import get_image_text
 
+
 def test_hello_world_image(image_hello_world):
+    """Read text from a black and white image"""
     text = get_image_text(image_hello_world)
     assert text.strip() == 'hello world'
-
-
