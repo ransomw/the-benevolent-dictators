919b12636c3f1b05c0002a8200d9343caf4a4244
BautistaDalmasso <80110679+BautistaDalmasso@users.noreply.github.com>
2023-09-08 14:37:57 -0300

    Add cli for our string encoder and decoder. (#25)
    * Add cli for encoding and decoding.
    
    * Add cli for generating ciphers.
    
    * Add seeded ciphers.

--- test/test_click_commands.py
+++ test/test_click_commands.py
@@ -6,7 +6,11 @@ import pytest
 from click.testing import CliRunner
 from PIL import Image
 
-from benevolent.cli import box_write, xor_code
+import benevolent.sub_cipher as sc
+from benevolent.cli import (
+    box_write, decode_with_cipher, encode_with_cipher, generate_cipher,
+    xor_code
+)
 
 TOLERABLE_ERROR_MARGIN = 2.0  # Eyeballed it, feel free to change if necessary.
 
@@ -58,3 +62,65 @@ def test_text_is_written(tmp_path, image1_hello_world_bmp):
 
     with Image.open(write_result_path) as result:
         assert equal_images_within_margin(image1_hello_world_bmp, result, TOLERABLE_ERROR_MARGIN)
+
+
+def test_encoding():
+    """Test encoding text from the cli."""
+    cipher_path = Path("test") / "ciphers" / "simple_sub_01.cipher"
+    runner = CliRunner()
+    result = runner.invoke(encode_with_cipher,
+                           ["hello world",
+                            "--cipher-path", str(cipher_path.resolve())])
+
+    assert result.exit_code == 0
+    assert result.output == "gpcch uhbcw\n"
+
+
+def test_decoding():
+    """Test decoding text from the cli."""
+    cipher_path = Path("test") / "ciphers" / "simple_sub_01.cipher"
+    runner = CliRunner()
+    result = runner.invoke(decode_with_cipher,
+                           ["gpcch uhbcw",
+                            "--cipher-path", str(cipher_path.resolve())])
+
+    assert result.exit_code == 0
+    assert result.output == "hello world\n"
+
+
+def test_seeded_encoding():
+    """Test encoding with a given seed."""
+    runner = CliRunner()
+    result = runner.invoke(encode_with_cipher,
+                           ["hello world",
+                            "--cipher-seed", 1234])
+
+    assert result.exit_code == 0
+    assert result.output == "gqllc icnlv\n"
+
+
+def test_seeded_decoding():
+    """Test decoding with a given seed."""
+    runner = CliRunner()
+    result = runner.invoke(decode_with_cipher,
+                           ["gqllc icnlv",
+                            "--cipher-seed", 1234])
+
+    assert result.exit_code == 0
+    assert result.output == "hello world\n"
+
+
+def test_seeded_cipher_generating(tmp_path):
+    """Test generating a cipher with a seed."""
+    path_expected_cipher = Path("test") / "ciphers" / "seeded_1234.cipher"
+    path_generated_cipher = Path(tmp_path) / "generated_1234.cipher"
+    runner = CliRunner()
+    result = runner.invoke(generate_cipher,
+                           [str(path_generated_cipher.resolve()),
+                            "--cipher-seed", 1234])
+
+    assert result.exit_code == 0
+
+    expected_cipher = sc.load_simple_sub_cipher(path_expected_cipher)
+    generated_cipher = sc.load_simple_sub_cipher(path_generated_cipher)
+    assert expected_cipher == generated_cipher
