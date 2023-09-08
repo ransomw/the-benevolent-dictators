3cadb74c25001abc861c461c0933f44fff6eed70
Bautista Dalmasso <j.bauti.d@gmail.com>
2023-09-08 18:04:35 -0300

    Refactor writer cli unit tests.

--- test/test_click_commands/test_click_commands.py
+++ test/test_click_commands/test_click_commands.py
@@ -1,35 +1,15 @@
 from pathlib import Path
-from test.conftest import IN_GITHUB_ACTIONS
 from test.utils import equal_images_within_margin
 
-import pytest
 from click.testing import CliRunner
 from PIL import Image
 
 import benevolent.sub_cipher as sc
 from benevolent.cli import (
-    benevolens, box_write, decode_with_cipher, encode_with_cipher,
-    generate_cipher
+    benevolens, decode_with_cipher, encode_with_cipher, generate_cipher
 )
 
 TOLERABLE_ERROR_MARGIN = 5.0  # Eyeballed it, feel free to change if necessary.
-
-
-@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Images are saved differently in GH actions env.")
-def test_text_is_written(tmp_path, image1_hello_world_bmp):
-    """Test writting to a bmp image."""
-    original_image_path = Path("test") / "images" / "acolchado.bmp"
-    write_result_path = tmp_path / "write_result.bmp"
-
-    CliRunner().invoke(box_write, [str(original_image_path.resolve()),
-                                   "hello world",
-                                   "--size", 36,
-                                   "-x", 0,
-                                   "-y", 0,
-                                   str(write_result_path.resolve())])
-
-    with Image.open(write_result_path) as result:
-        assert equal_images_within_margin(image1_hello_world_bmp, result, TOLERABLE_ERROR_MARGIN)
 
 
 def test_encoding():
