11bc7ad07d6f9cf86e4cafcda4e16c479c17dfe7
Bautista Dalmasso <j.bauti.d@gmail.com>
2023-09-08 18:05:55 -0300

    See if gh actions behaves with equal_images_within_margin.

--- test/test_click_commands/test_writer_commands.py
+++ test/test_click_commands/test_writer_commands.py
@@ -1,8 +1,6 @@
 from pathlib import Path
-from test.conftest import IN_GITHUB_ACTIONS
 from test.utils import equal_images_within_margin
 
-import pytest
 from click.testing import CliRunner
 from PIL import Image
 
@@ -11,7 +9,6 @@ from benevolent.cli import box_write
 TOLERABLE_ERROR_MARGIN = 5.0  # Eyeballed it, feel free to change if necessary.
 
 
-@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Images are saved differently in GH actions env.")
 def test_text_is_written(tmp_path, image1_hello_world_bmp):
     """Test writting to a bmp image."""
     original_image_path = Path("test") / "images" / "acolchado.bmp"
