f77b2f83ac7191a389e1e4dbfc20b955228cac51
Bautista Dalmasso <j.bauti.d@gmail.com>
2023-09-08 21:05:23 -0300

    Add a test with a clean image.

--- test/test_translate_chunks.py
+++ test/test_translate_chunks.py
@@ -10,3 +10,16 @@ def test_chunk_translation(image_messy_hello_world):
                      ).save("prueba2.jpg")
 
     assert "h" == sc.encode_simple_sub_cipher(sc.generate_seeded_sub_cipher("abcd"), "hello world")
+
+
+def test_clean_translation(image_clean_hello_world):
+    """Test translating a clean image in chunks.
+
+    I just wanted to see our programm nailing a translation for once XD
+    """
+    translate_chunks(image_clean_hello_world,
+                     sc.generate_seeded_sub_cipher("abcd"),
+                     [(231, 166, 440, 251), (1108, 555, 1318, 638)]
+                     ).save("prueba3.jpg")
+
+    assert "h" == sc.encode_simple_sub_cipher(sc.generate_seeded_sub_cipher("abcd"), "hello world")
