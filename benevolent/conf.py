808ab751c062b4bcdfdb7ef34d6f60fa5dc1d900
BautistaDalmasso <80110679+BautistaDalmasso@users.noreply.github.com>
2023-09-09 23:46:38 -0300

    Add functionality to automatically create config file. (#34)

--- benevolent/conf.py
+++ benevolent/conf.py
@@ -2,12 +2,17 @@ from configparser import ConfigParser
 from pathlib import Path
 from typing import Optional
 
-from appdirs import user_config_dir
-
 
 def config_file_path() -> Path:
     """Returns the path to the config file"""
-    return Path(user_config_dir("benevolent", "tbd")) / "benevolent.conf"
+    return Path("benevolent.conf")
+
+
+def create_config(tesseract_path: str) -> None:
+    """Creates the config file for tesseract."""
+    with open(config_file_path(), "w") as config_file:
+        config_file.write("[tesseract]\n")
+        config_file.write(f"path={tesseract_path}")
 
 
 def get_config() -> Optional[ConfigParser]:
