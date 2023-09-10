32e1a9c55d3069322039278abaee259ef2b63d10
BautistaDalmasso <80110679+BautistaDalmasso@users.noreply.github.com>
2023-09-09 21:46:22 -0300

    Add cli for translating an image in segments. (#32)
    * Improve documentation a little.
    
    * Add cli for translating in segments.

--- benevolent/cli.py
+++ benevolent/cli.py
@@ -5,6 +5,7 @@ from PIL import Image
 
 import benevolent.sub_cipher as sc
 from benevolent.conf import config_file_path
+from benevolent.translate_chunks import translate_chunks
 from benevolent.translator import translate_image
 from benevolent.writer import Writer
 from benevolent.xor_enconder import create_xor_code
@@ -30,8 +31,8 @@ def print_config_path():
 def xor_code(path1, path2, path_out):
     """Exclusive or (xor) two images together
 
-    path1 is the path of the first image to use.
-    path2 is the path of the second image to use.
+    path1 is the path of the first image to use.\n
+    path2 is the path of the second image to use.\n
     path-out is the path where the resulting image will be saved.
     """
     with Image.open(path1) as image1, Image.open(path2) as image2:
@@ -49,8 +50,8 @@ def xor_code(path1, path2, path_out):
 def box_write(image_path, text, size, x, y, path_out):
     """Write text to an image inside a box.
 
-    image-path is the path of the image that will be written in.
-    text is the text to write.
+    image-path is the path of the image that will be written in.\n
+    text is the text to write.\n
     path-out is the path to save the image with the written text to.
     """
     with Image.open(image_path) as image:
@@ -125,12 +126,53 @@ def generate_cipher(cipher_path, cipher_seed=None):
 def benevolens(image_path_in, cipher_path, cipher_seed, format, image_path_out):
     """Reads text from an image and decodes it. Either provide a cipher file path or a cipher seed.
 
-    image-path-in is the path where the image you want to decode is.
+    image-path-in is the path where the image you want to decode is.\n
     image-path-out the path to save the decoded image in.
     """
     img = Image.open(image_path_in)
 
     translated_image = translate_image(img, _get_cipher(cipher_path, cipher_seed))
+    if format:
+        translated_image.save(image_path_out, bitmap_format=[format])
+    else:
+        translated_image.save(image_path_out)
+
+
+@cli.command()
+@click.argument("image-path-in", required=True, type=click.Path(exists=True))
+@click.option("--cipher-path", required=False, type=click.Path(exists=True),
+              help="The path where the cipher file is located, when decoding with one.")
+@click.option("--cipher-seed", required=False, help="The seed to use for decoding, when decoding with one.")
+@click.option("--format", required=False, help="Valid PIL bitmap format.")
+@click.argument("segment", required=True, type=(int, int, int, int))
+@click.argument("image-path-out", required=True, type=click.Path(exists=False))
+def benevolens_segments(image_path_in, cipher_path, cipher_seed, format, segment, image_path_out):
+    """Reads text from a segment of an image and decodes it. Either provide a cipher file path or a cipher seed.
+
+    image-path-in is the path where the image you want to decode is.\n
+    segment is the coordinates that create the segment of the image that you want to translate.
+    Provide the segment by giving: top left x coordinate, top left y coordinate, bottom right x coordinate and
+    bottom right y coordinate.\n
+    image-path-out the path to save the decoded image in.\n
+
+    Example:
+    benevolens benevolens-segments \\images\\img.jpg --cipher-seed xxxx x1 y1 x2 y2 \\images\\translated_img.jpg\n
+
+    \b
+    Your image:
+    \b
+    ___________________________________________
+    |                                         |
+    | (x1, y1) *_____________                 |
+    |          |             |                |
+    |          | coded text  |                |
+    |          |             |                |
+    |          ‾‾‾‾‾‾‾‾‾‾‾‾‾‾*(x2, y2)        |
+    |                                         |
+    ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+    """
+    img = Image.open(image_path_in)
+    translated_image = translate_chunks(img, _get_cipher(cipher_path, cipher_seed), [segment])
     if format:
         translated_image.save(image_path_out, bitmap_format=[format])
     else:
--- test/test_click_commands/test_benevolens.py
+++ test/test_click_commands/test_benevolens.py
@@ -4,7 +4,7 @@ from test.utils import equal_images_within_margin
 from click.testing import CliRunner
 from PIL import Image
 
-from benevolent.cli import benevolens
+from benevolent.cli import benevolens, benevolens_segments
 
 TOLERABLE_ERROR_MARGIN = 5.0  # Eyeballed it, feel free to change if necessary.
 
@@ -25,3 +25,22 @@ def test_benevolens(tmp_path, image_hello_world_decoded):
 
     with Image.open(path_result) as result:
         assert equal_images_within_margin(image_hello_world_decoded, result, TOLERABLE_ERROR_MARGIN)
+
+
+def test_benevolens_segments(tmp_path, image_messy_hello_world_half_decoded):
+    """Test image reading, decoding and writing."""
+    path_original_image = Path("test") / "images" / "hello_world_encoded_2.jpg"
+    path_result = Path(tmp_path) / "result.bmp"
+
+    result = CliRunner().invoke(benevolens_segments,
+                                [str(path_original_image.resolve()),
+                                 "--cipher-seed", "abcd",
+                                 "--format", "bmp",
+                                 "141", "91", "280", "145",
+                                 str(path_result.resolve())
+                                 ])
+
+    assert result.exit_code == 0
+
+    with Image.open(path_result) as result:
+        assert equal_images_within_margin(image_messy_hello_world_half_decoded, result, TOLERABLE_ERROR_MARGIN)
