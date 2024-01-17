ade73b1948c34285d5111a2c2f9ef0f30acde9ce
Ransom Williams <auvergnerw@gmail.com>
2024-01-17 13:28:57 -0600

    qa image uploads

--- benevolent/web.py
+++ benevolent/web.py
@@ -1,31 +1,53 @@
+import base64
+from pathlib import Path
 import os.path
+from typing import Set
+from os.path import splitext
 
 from quart import Quart, request, flash, redirect, url_for
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER: str = "../web/uploads"
-TEMPLATE_FOLDER: str = "../web/templates"
-ALLOWED_EXTENSIONS: set[str, ...] = {"bmp"}
+UPLOAD_FOLDER: Path = Path(__file__).parent.parent / "uploads"
+TEMPLATE_FOLDER: Path = Path(__file__).parent / "templates"
+ALLOWED_EXTENSIONS: Set[str] = {"bmp", "png"}
 
 app = Quart(
     __name__,
-    template_folder = TEMPLATE_FOLDER
+    template_folder = str(TEMPLATE_FOLDER)
 )
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["UPLOAD_FOLDER"] = str(UPLOAD_FOLDER)
 
 
 def allowed_file(filename: str):
-    return "." in filename and filename.split(".")[1] in ALLOWED_EXTENSIONS
+    (_, ext) = splitext(filename)
+    return ext[1:] in ALLOWED_EXTENSIONS
 
 
-@app.route("/", methods = ["GET", "POST"])
-async def root():
+@app.route("/box-write")
+async def box_write():
+    return 'unimplemented'
+
+
+@app.route("/uploaded/<filename>")
+async def uploaded_file(filename):
+    (_, ext) = splitext(filename)
+    filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+    with open(filepath, "rb") as fh:
+        data = base64.b64encode(fh.read())
+    
+    return f"""
+    <img src="data:image/{ext[1:]}; base64, {data.decode('utf-8')}">
+    """
+
+
+@app.route("/upload", methods = ["GET", "POST"])
+async def upload():
     if request.method == "POST":
-        if "file" not in request.files:
+        if "file" not in (await request.files):
             await flash("No file part")
             return redirect(request.url)
-        print(request.files)
-        file = request.files["file"]
+        print(await request.files)
+        file = (await request.files)["file"]
 
         if file.filename == "":
             await flash("No selected file")
@@ -33,8 +55,10 @@ async def root():
 
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            await file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
             return redirect(url_for("uploaded_file", filename = filename))
+        else:
+            return "filename not allowed by extension"
 
     return '''
         <!doctype html>
@@ -45,6 +69,7 @@ async def root():
           <input type=submit value=Upload>
         </form>
         '''
+
 
 if __name__ == "__main__":
     app.run(debug = True, port = 8080)
